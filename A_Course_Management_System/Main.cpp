72a0a66aba653183854212270103403f3b2e2f50
NguyenNgocDang <ngocdang240604@gmail.com>
2023-03-23 00:06:52 +0700

    Change Main.cpp

--- A_Course_Management_System/Main.cpp
+++ A_Course_Management_System/Main.cpp
@@ -2,20 +2,6 @@
 int main()
 {
 	Invisible_Cursor();
-	Pair ans;
-	while (true)
-	{
-		ans = Show_Mouse_Pos();
-		if (ans.y == 0 && ans.x>=0 && ans.x<6)
-		{
-			SetColor("black", "blue");
-		}
-		else
-		{
-			ResetColor();
-		}
-		Gotoxy(0, 0);
-		cout << "Hello";
-	}
+	
 	return 0;
 }
